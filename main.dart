737d94634ec0517a94469c95f9085ff4503427d1
Riswan <shamseernpm@gmail.com>
2026-09-24 15:13:54 +0530

    day 1 just practice dart

--- main.dart
+++ main.dart
@@ -1,5 +1,56 @@
-void main(){
-    print("Hello World");
-    print("today just start flutter");
-    print("helloooo");
+
+abstract class Human {
+    void walk(){
+        print("walk style");
+    }
 }
+
+class Iam extends Human{
+    void walk(){
+        print("good walk");
+    }
+}
+
+
+greet(){
+    Iam riswan=Iam();
+
+    riswan.walk();
+}
+
+
+
+//polymorphism 
+
+class Animal {
+    void sound(){
+        print("animal sound");
+    }
+}
+
+class Dog extends Animal {
+    // @override
+    void sound(){
+        print("dog sound");
+    }
+}
+
+// class Cat extends Animal {
+
+//     // @override
+//     void sound(){
+//         print("cat sound");
+//     }
+// }
+
+void main(){
+   greet();
+
+
+    Dog dog=Dog();
+    // Cat cat=Cat();
+
+    dog.sound();
+    // cat.sound();
+ }
+
